c07086951662dae3d0f5be66fe99156242f2c65b
Hasanul Bari <39557895+Hasanul-Bari@users.noreply.github.com>
2020-08-29 23:12:47 +0600

    Initial commit

---
+++ src/jdbc/JDBC.java
@@ -0,0 +1,42 @@
+package jdbc;
+
+/**
+ *
+ * @author Hasan
+ */
+
+import java.sql.*;
+
+public class JDBC {
+
+   
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+        
+        String url="jdbc:mysql://localhost/jdbc";
+        String userName="root";
+        String Password="";
+        
+        String query="SELECT Name from student where Id=1702065";
+        
+        Class.forName("com.mysql.jdbc.Driver");
+        
+        Connection con=DriverManager.getConnection(url,userName,Password);
+        Statement st=con.createStatement();
+        
+        ResultSet rs=st.executeQuery(query);
+        
+        rs.next();
+        
+        String name=rs.getString("Name");
+        
+        System.out.println(name);
+        
+        st.close();
+        con.close();
+        
+        
+        
+    }
+    
+}
